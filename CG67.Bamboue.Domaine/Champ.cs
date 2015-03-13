a3350f2d97daf9619f5e6b02f42d1ceb239a1f36
flibidi67 <jpdu67@hotmail.fr>
2015-03-13 12:27:23 +0100

    finition classe Champ

--- CG67.Bamboue.Domaine/Champ.cs
+++ CG67.Bamboue.Domaine/Champ.cs
@@ -8,7 +8,6 @@ namespace CG67.Bamboue.Domaine
 {
     public class Champ
     {
-
         #region Propriété
 
         private int id;
@@ -25,7 +24,9 @@ namespace CG67.Bamboue.Domaine
         private String commentaire;
 
         #endregion
-       
+
+        #region Constructeur
+
         public Champ(int id, String nom, String inseeCommune, String lieuDit, int habitation,
             int perimetreRapproche, int perimetreEloigne, int captageEau, int zoneInondable,
             int coursEau, String commentaire, String commune, String nomExploitation)
@@ -44,9 +45,12 @@ namespace CG67.Bamboue.Domaine
             this.nomExploitation = nomExploitation;
         }
 
+        #endregion
+
         #region Accesseurs
 
-        public int Id{
+        public int Id
+        {
             get { return this.id; }
             set { this.id = value; }
         }
@@ -54,10 +58,72 @@ namespace CG67.Bamboue.Domaine
         public String Nom
         {
             get { return this.nom; }
-            set { this.nom = value; }
+            set { this.nom = value == null ? String.Empty : value.Trim(); }
+        }
+
+        public String LieuDit
+        {
+            get { return this.lieuDit; }
+            set { this.lieuDit = value == null ? String.Empty : value.Trim(); }
+        }
+
+        public int Habitation
+        {
+            get { return this.habitation; }
+            set { this.habitation = value; }
+        }
+
+        public int PerimetreProtectionEloigne
+        {
+            get { return this.perimetreProtectionEloigne; }
+            set { this.perimetreProtectionEloigne = value; }
+        }
+
+        public int PerimetreProtectionRapproche
+        {
+            get { return this.perimetreProtectionRapproche; }
+            set { this.perimetreProtectionRapproche = value; }
+        }
+
+        public int CaptageEau
+        {
+            get { return this.captageEau; }
+            set { this.captageEau = value; }
+        }
+
+        public int ZoneInondable
+        {
+            get { return this.zoneInondable; }
+            set { this.zoneInondable = value; }
+        }
+
+        public int CoursEau
+        {
+            get { return this.coursEau; }
+            set { this.coursEau = value; }
+        }
+
+        public String Commentaire
+        {
+            get { return this.commentaire; }
+            set { this.commentaire = value == null ? String.Empty : value.Trim(); }
+        }
+
+        public String Commune
+        {
+            get { return this.commune; }
+            set { this.commune = value == null ? String.Empty : value.Trim(); }
+        }
+
+        public String NomExploitation
+        {
+            get { return this.nomExploitation; }
+            set
+            {
+                this.nomExploitation = value == null ? String.Empty : value.Trim();
+            }
         }
 
         #endregion
-
     }
 }
