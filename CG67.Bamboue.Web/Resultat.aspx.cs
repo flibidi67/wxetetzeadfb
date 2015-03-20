41473d65cbd893f5d83b5f15e68d0e34f96713bb
Overpunk <robinpautler@hotmail.fr>
2015-03-20 10:55:56 +0100

    Ajout des pages accueil et resultat

---
+++ CG67.Bamboue.Web/Resultat.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CG67.Bamboue.Web
+{
+    public partial class Résultat : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
