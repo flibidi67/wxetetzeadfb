51894aa1988620c361971017f28ec7726f897f8b
flibidi67 <jpdu67@hotmail.fr>
2015-03-20 11:17:05 +0100

    export xml

--- CG67.Bamboue.Utilities/Utilitaire.cs
+++ CG67.Bamboue.Utilities/Utilitaire.cs
@@ -2,6 +2,8 @@
 using System.Data;
 using System.Data.Common;
 using System.Configuration;
+using CG67.Bamboue.Domaine;
+using System.Xml;
 
 namespace CG67.Bamboue.Utilities
 {
@@ -39,7 +41,48 @@ namespace CG67.Bamboue.Utilities
         #endregion
 
         #region Export XML
+        public static void ExportXML(Champ champ)
+        {
+            XmlTextWriter writer = new XmlTextWriter(champ.Nom + ".xml", System.Text.Encoding.UTF8);
+            writer.WriteStartDocument(true);
 
+            writer.Formatting = Formatting.Indented;
+            writer.Indentation = 2;
+            writer.WriteStartElement("Champ");
+            
+            CreateNode("Nom", champ.Nom, writer);
+            CreateNode("Commune", champ.Commune, writer);
+            CreateNode("Lieu dit", champ.LieuDit, writer);
+
+            writer.WriteStartElement("Contrainte");
+
+            CreateNode("Habitation", OuiOuNon(champ.Habitation), writer);
+            CreateNode("Captage d'eau", OuiOuNon(champ.CaptageEau), writer);
+            CreateNode("ZoneInondable", OuiOuNon(champ.ZoneInondable), writer);
+            CreateNode("Cours d'eau", OuiOuNon(champ.CoursEau), writer);
+
+            writer.WriteEndElement();//fin balise contrainte
+            writer.WriteEndDocument();//fin balise champ
+            writer.Close();
+        }
+
+        public static void CreateNode(string nomBalise, string valeur, XmlTextWriter writer)
+        {
+            writer.WriteStartElement(nomBalise);
+            writer.WriteString(valeur);
+            writer.WriteEndElement();
+        }
+        public static String OuiOuNon(int v)
+        {
+            if (v == 0)
+            {
+                return "Non";
+            }
+            else
+            {
+                return "Oui";
+            }
+        }
         #endregion
     }
 }
