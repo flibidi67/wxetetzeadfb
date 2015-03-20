e2b9bbae4c2cfa8144c34ca8361f24eea0a11887
Arcahyxe <quentin_dell@live.fr>
2015-03-20 10:42:10 +0100

    Ajout de la classe Installation dans domaine

---
+++ CG67.Bamboue.Domaine/Installation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG67.Bamboue.Domaine
+{
+    class Installation
+    {
+        #region Propriétés
+        private int idInstallation;
+        private int idTypeInstallation;
+        private int idAdresse;
+        private string nom;
+        private int capaciteTraitement;
+        private string commentaire;
+        #endregion
+
+        #region Constructeur
+        public Installation(int idInstallation, int idTypeInstallation, int idAdresse, string nom, int capaciteTraitement, string commentaire)
+        {
+            this.idInstallation = idInstallation;
+            this.idTypeInstallation = idTypeInstallation;
+            this.idAdresse = idAdresse;
+            this.nom = nom;
+            this.capaciteTraitement = capaciteTraitement;
+            this.commentaire = commentaire;
+        }
+        #endregion 
+
+        #region Accesseurs
+        public int IdInstallation
+        {
+            get { return this.idInstallation; }
+            set { this.idInstallation = value; }
+        }
+
+        public int IdTypeInstallation
+        {
+            get { return this.idTypeInstallation; }
+            set { this.idTypeInstallation = value; }
+        }
+        public int IdAdresse
+        {
+            get { return this.idAdresse; }
+            set { this.idAdresse = value; }
+        }
+
+        public String Nom
+        {
+            get { return this.nom; }
+            set { this.nom = value == null ? String.Empty : value.Trim(); }
+        }
+
+        public int CapaciteTraitement
+        {
+            get { return this.capaciteTraitement; }
+            set { this.capaciteTraitement = value; }
+        }
+
+        public String Commentaire
+        {
+            get { return this.commentaire; }
+            set { this.commentaire = value == null ? String.Empty : value.Trim(); }
+        }
+        #endregion
+
+    }
+}
