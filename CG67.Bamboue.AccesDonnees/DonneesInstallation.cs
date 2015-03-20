b345f3b30098be889251258250f9f0a056d2f6d3
Overpunk <robinpautler@hotmail.fr>
2015-03-20 11:32:32 +0100

    données installation

--- CG67.Bamboue.AccesDonnees/DonneesInstallation.cs
+++ CG67.Bamboue.AccesDonnees/DonneesInstallation.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CG67.Bamboue.Utilities;
+using System.Configuration;
+using System.Data;
 
 namespace CG67.Bamboue.AccesDonnees
 {
     class DonneesInstallation
     {
+        ConnectionStringSettings connec = new ConnectionStringSettings("bamboue", ConfigurationManager.ConnectionStrings["bamboueConnectionString"].ConnectionString);
+
+        public DataSet GetToutesInstallations()
+        {
+            String query = "SELECT Nom FROM Installation";
+            DataSet ds = Utilities.Utilitaire.BuildDataSet(query, connec);
+            return ds;
+        }
     }
 }
